a5beb37df43e20d22e79f1c92d91c18a33d237cf
Pradeep Rajkumar <>
2018-05-24 21:18:47 +0530

    All longest strings in array arcade program completed.

--- CodeFights/Arcade/All Longest Strings.playground/Contents.swift
+++ CodeFights/Arcade/All Longest Strings.playground/Contents.swift
@@ -1,4 +1,34 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
+/*
+func allLongestStrings(inputArray: [String]) -> [String] {
+    var countArray: [Int] = []
+    var resultArray: [String] = []
+    var maxCount = 0
+    countArray = inputArray.map {
+        maxCount = $0.characters.count > maxCount ? $0.characters.count : maxCount
+        return $0.characters.count
+    }
+    
+    return inputArray.filter({ $0.characters.count == maxCount })
+}
+ */
 
+func allLongestStrings(inputArray: [String]) -> [String] {
+    var countArray: [Int] = []
+    var resultArray: [String] = []
+    var maxCount = 0
+    countArray = inputArray.map {
+        maxCount = $0.characters.count > maxCount ? $0.characters.count : maxCount
+        return $0.characters.count
+    }
+    for index in 0..<inputArray.count {
+        if countArray[index] == maxCount {
+         resultArray.append(inputArray[index])
+        }
+    }
+    return resultArray
+}
+
+print(allLongestStrings(inputArray: ["aba", "aa", "ad", "vcd", "aba"]))
