7ab1bb2944d0ccdea7923fd6e4a43789dd0ea467
Pradeep Rajkumar <>
2018-05-25 12:07:22 +0530

    Converted the example to English.

--- Others/Decodable example.playground/Contents.swift
+++ Others/Decodable example.playground/Contents.swift
@@ -6,11 +6,11 @@ let data: Data
 struct Organisation {
     struct Employee: Decodable {
         var designation: String
-        var scheda_tecnica: [String:String]?
-        var descrizione: String
-        var iscrizione: String
-        var materiali: String
-        var notizie_storiche: String?
+        var tecnical: [String:String]?
+        var description: String
+        var inscription: String
+        var materials: String
+        var notice: String?
         var image: String?
     }
     var employees: [Employee]
@@ -30,11 +30,11 @@ extension Organisation: Decodable {
         init?(intValue: Int) {
             return nil
         }
-        static let descrizione = EmployeeKey(stringValue: "descrizione")
-        static let scheda_tecnica = EmployeeKey(stringValue: "scheda_tecnica")
-        static let iscrizione = EmployeeKey(stringValue: "iscrizione")
-        static let materiali = EmployeeKey(stringValue: "materiali")
-        static let notizie_storiche = EmployeeKey(stringValue: "notizie_storiche")
+        static let description = EmployeeKey(stringValue: "description")
+        static let tecnical = EmployeeKey(stringValue: "tecnical")
+        static let inscription = EmployeeKey(stringValue: "inscription")
+        static let materials = EmployeeKey(stringValue: "materials")
+        static let notice = EmployeeKey(stringValue: "notice")
         static let image = EmployeeKey(stringValue: "image")
     }
     
@@ -45,14 +45,14 @@ extension Organisation: Decodable {
             
             let employeeContainer = try container.nestedContainer(keyedBy: EmployeeKey.self, forKey: key)
             
-            let descrizione = try employeeContainer.decode(String.self, forKey: EmployeeKey.descrizione!)
-            let scheda_tecnica = try employeeContainer.decode([String: String].self, forKey: EmployeeKey.scheda_tecnica!)
-            let iscrizione = try employeeContainer.decode(String.self, forKey: EmployeeKey.iscrizione!)
-            let materiali = try employeeContainer.decode(String.self, forKey: EmployeeKey.materiali!)
-            let notizie_storiche = try employeeContainer.decode(String.self, forKey: EmployeeKey.notizie_storiche!)
+            let description = try employeeContainer.decode(String.self, forKey: EmployeeKey.description!)
+            let tecnical = try employeeContainer.decode([String: String].self, forKey: EmployeeKey.tecnical!)
+            let inscription = try employeeContainer.decode(String.self, forKey: EmployeeKey.inscription!)
+            let materials = try employeeContainer.decode(String.self, forKey: EmployeeKey.materials!)
+            let notice = try employeeContainer.decode(String.self, forKey: EmployeeKey.notice!)
             let image = try employeeContainer.decode(String.self, forKey: EmployeeKey.image!)
             
-            let employee = Employee(designation: key.stringValue, scheda_tecnica: scheda_tecnica, descrizione: descrizione, iscrizione: iscrizione, materiali: materiali, notizie_storiche: notizie_storiche, image: image)
+            let employee = Employee(designation: key.stringValue, tecnical: tecnical, description: description, inscription: inscription, materials: materials, notice: notice, image: image)
             employees.append(employee)
         }
         self.init(employees: employees)
@@ -68,7 +68,7 @@ do {
     let decoder = JSONDecoder()
     if let decodedOrganisation = try? decoder.decode(Organisation.self, from: data) {
         for employee in decodedOrganisation.employees {
-            print(employee.designation, employee.descrizione, employee.iscrizione, employee.scheda_tecnica, employee.image, employee.notizie_storiche)
+            print("\n",employee.designation, "\n", employee.description, employee.inscription, "\n", employee.tecnical, "\n", employee.materials, employee.notice, employee.image)
             
         }
     }
