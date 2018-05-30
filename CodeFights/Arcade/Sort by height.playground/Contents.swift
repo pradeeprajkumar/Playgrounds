c8f700d80a182df3bfe8bc7bb637713386a98fe0
Pradeep Rajkumar <>
2018-05-30 16:55:25 +0530

    Minor changes.

--- CodeFights/Arcade/Sort by height.playground/Contents.swift
+++ CodeFights/Arcade/Sort by height.playground/Contents.swift
@@ -1,9 +1,6 @@
 import Foundation
 
 func sortByHeight(a: [Int]) -> [Int] {
-    
-    let startTime = Date.timeIntervalSinceReferenceDate
-    
     //Space complexity: (2n)
 
     //We created one result array, and one sorted array
@@ -29,15 +26,12 @@ func sortByHeight(a: [Int]) -> [Int] {
             positiveIndex += 1
         }
     }
-    
-    print("\nMy code \n Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
-    print(" Total time complexity: O(n+n*log(n))")
+    //Total time complexity: O(n+n*log(n))"
     return resultArray
 }
 
 
 func sortByHeight2(a: [Int]) -> [Int] {
-    let startTime = Date.timeIntervalSinceReferenceDate
     //Time complexity for Filtering: O(n)
     //Time complexity for Sorting: O(n*log(n))
     var sorted = a.filter({$0 != -1}).sorted()
@@ -49,13 +43,51 @@ func sortByHeight2(a: [Int]) -> [Int] {
             sorted.insert(-1, at: i)
         }
     }
-    print("\nOnline code \n Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
-    print(" Total time complexity: O(n+n*log(n)+nˆ2)")
+    
+    //Total time complexity: O(n+n*log(n)+nˆ2)"
     return sorted
 }
 
 
-let array = [-1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60]
+let array = [-1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60]
 
-print(sortByHeight(a: array))
-print(sortByHeight2(a: array))
+
+func sort(array:[Int]) -> [Int] {
+    var indexes : [Int] = []
+    for (index,element) in array.enumerated() {
+        if element == -1 {
+            indexes.append(index)
+        }
+    }
+    var newArray = array.filter {$0 != -1}.sorted()
+    indexes.forEach { (index) in
+        newArray.insert(-1, at: index)
+    }
+    return newArray
+}
+
+//print(sort(array: [-1, 150, 190, 170, -1, -1, 160, 180]))
+
+//Expected output: [-1, 150, 160, 170, -1, -1, 180, 190]
+
+
+let times = 100
+var startTime = Date.timeIntervalSinceReferenceDate
+for _ in  0..<times {
+    sort(array: array)
+}
+print("\nSanjay code \n Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
+
+startTime = Date.timeIntervalSinceReferenceDate
+for _ in  0..<times {
+    sortByHeight(a: array)
+}
+print("\nPradeep code \n Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
+
+startTime = Date.timeIntervalSinceReferenceDate
+for _ in  0..<times {
+    sortByHeight2(a: array)
+}
+print("\nOnline code \n Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
+
+
