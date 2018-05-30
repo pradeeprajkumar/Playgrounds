829bd656d2f26ff63268677d0b3d3ceb33f2caf2
Pradeep Rajkumar <>
2018-05-30 12:39:00 +0530

    Minor changes.

--- CodeFights/Arcade/Sort by height.playground/Contents.swift
+++ CodeFights/Arcade/Sort by height.playground/Contents.swift
@@ -5,9 +5,10 @@ func sortByHeight(a: [Int]) -> [Int] {
     let startTime = Date.timeIntervalSinceReferenceDate
     
     //Space complexity: (2n)
+
     //We created one result array, and one sorted array
     var resultArray: [Int] = []
-    //Time complexity: O(n*Log(n))
+    //Time complexity: O(n*log(n))
     var sortedArray = a.sorted()
     var positiveIndex = 0
     while positiveIndex < sortedArray.count {
@@ -29,24 +30,32 @@ func sortByHeight(a: [Int]) -> [Int] {
         }
     }
     
-    print("Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
+    print("\nMy code \n Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
+    print(" Total time complexity: O(n+n*log(n))")
     return resultArray
 }
 
 
 func sortByHeight2(a: [Int]) -> [Int] {
     let startTime = Date.timeIntervalSinceReferenceDate
+    //Time complexity for Filtering: O(n)
+    //Time complexity for Sorting: O(n*log(n))
     var sorted = a.filter({$0 != -1}).sorted()
 
+    //Time complexity for inserting: O(nˆ2)
+    //Worst case, if we insert -1 everytime in the beginning, we will end up at nˆ2
     for i in 0..<a.count {
         if a[i] == -1 {
             sorted.insert(-1, at: i)
         }
     }
-    print("Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
+    print("\nOnline code \n Time taken: \(Date.timeIntervalSinceReferenceDate - startTime) seconds")
+    print(" Total time complexity: O(n+n*log(n)+nˆ2)")
     return sorted
 }
 
+
 let array = [-1, 150, 190, 170, -1, 8, -1, 160, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60, -1, 3, 1, 2, 5, 8, -1, 160, 180, 7, 8, 160, 180, 7, -1, 8, 4, -1, 60, 53, -1, 11, 22, -1, 85, 48, 60]
-print("Online: \(sortByHeight2(a: array))\n")
-print("My code: \(sortByHeight(a: array))\n")
+
+print(sortByHeight(a: array))
+print(sortByHeight2(a: array))
