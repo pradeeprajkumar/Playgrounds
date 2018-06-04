b43007928e04918a1b1114393b420fce6e755b71
Pradeep Rajkumar <>
2018-06-04 13:00:33 +0530

    Optimised my code, using divisor.

--- CodeFights/Arcade/Avoid Obstacles.playground/Contents.swift
+++ CodeFights/Arcade/Avoid Obstacles.playground/Contents.swift
@@ -1,21 +1,6 @@
 import Foundation
 
-/*
-//Online code:
-func avoidObstacles(inputArray n: [Int]) -> Int {
-    var r = 2
-    o: while true {
-        for i in n {
-            if i%r < 1 {
-                r += 1
-                continue o
-            }
-        }
-        break
-    }
-    return r
-}
-*/
+
 //My Code
 func avoidObstacles(inputArray: [Int]) -> Int {
     var hopPointsArray: [Int] = []
@@ -47,5 +32,59 @@ func avoidObstacles(inputArray: [Int]) -> Int {
     return value
 }
 
-//print(avoidObstacles(inputArray: [19, 32, 11, 23]))
-print(avoidObstacles(inputArray: [3,5,7,9,11]))
+//Online code:
+func avoidObstacles1(inputArray n: [Int]) -> Int {
+    var r = 2
+    o: while true {
+        for i in n {
+            if i%r < 1 {
+                r += 1
+                continue o
+            }
+        }
+        break
+    }
+    return r
+}
+
+
+
+var startTime = Date.timeIntervalSinceReferenceDate
+//print("My code")
+//print(avoidObstacles(inputArray: [3,5,7,9,11]))
+//print(Date.timeIntervalSinceReferenceDate-startTime)
+////
+//startTime = Date.timeIntervalSinceReferenceDate
+//print("Online code")
+//print(avoidObstacles1(inputArray: [3,5,7,9,11]))
+//print(Date.timeIntervalSinceReferenceDate-startTime)
+
+
+
+func avoidObstacles2(inputArray: [Int]) -> Int {
+    var divisor = 2
+    var divisibleFound = false
+    let sortedArray = inputArray.sorted()
+    while divisor < sortedArray.last!+1 {
+        for index in 0..<sortedArray.count {
+            if sortedArray[index] % divisor == 0 {
+                divisor += 1
+                divisibleFound = true
+                break
+            } else {
+                divisibleFound = false
+            }
+        }
+        if divisibleFound == false {
+            return divisor
+        }
+    }
+    return divisor
+}
+
+
+startTime = Date.timeIntervalSinceReferenceDate
+print("My modified code")
+//print(avoidObstacles2(inputArray: [3,5,7,9,11]))
+print(avoidObstacles2(inputArray: [1, 4, 10, 6, 2]))
+print(Date.timeIntervalSinceReferenceDate-startTime)
