45b63961971549276572888efdb967ba23156552
Pradeep Rajkumar <>
2018-07-06 14:43:15 +0530

    Problem statement saved.

---
+++ CodeFights/Tournaments/Sort by Length.playground/Contents.swift
@@ -0,0 +1,40 @@
+/*
+ Question:
+ Given an array of strings, sort them in the order of increasing lengths. If two strings have the same length, their relative order must be the same as in the initial array.
+ 
+ Example
+ 
+ For
+ 
+ inputArray = ["abc",
+ "",
+ "aaa",
+ "a",
+ "zz"]
+ the output should be
+ 
+ sortByLength(inputArray) = ["",
+ "a",
+ "zz",
+ "abc",
+ "aaa"]
+ Input/Output
+ 
+ [execution time limit] 20 seconds (swift)
+ 
+ [input] array.string inputArray
+ 
+ A non-empty array of strings.
+ 
+ Guaranteed constraints:
+ 3 ≤ inputArray.length ≤ 100,
+ 0 ≤ inputArray[i].length ≤ 100.
+ 
+ Input: ["zz", "", "bb","ccc","cc"]
+ Output: ["", "zz", "bb", "cc", "ccc"]
+ */
+func sortByLength(inputArray: [String]) -> [String] {
+    
+}
+
+print(sortByLength(inputArray: []))
