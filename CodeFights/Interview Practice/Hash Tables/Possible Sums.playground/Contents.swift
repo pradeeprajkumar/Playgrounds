bbd1cbfb539514a6d8a0de04ca9639ee28102e2d
Pradeep Rajkumar <>
2018-07-12 00:59:40 +0530

    Solved problem for 1s count.

--- CodeFights/Interview Practice/Hash Tables/Possible Sums.playground/Contents.swift
+++ CodeFights/Interview Practice/Hash Tables/Possible Sums.playground/Contents.swift
@@ -1,22 +1,22 @@
+
 func possibleSums(coins: [Int], quantity: [Int]) -> Int {
     var sumsDictionary = [Int:[Int]]()
     for (coin, coinQuantity) in zip(coins, quantity) {
-        var numberOfCoins = 1
-        while numberOfCoins <= coinQuantity {
-            for key in sumsDictionary.keys {
-                if key == coin {
-                    sumsDictionary[key] = sumsDictionary[key]! + [coin * numberOfCoins]
-                } else {
-                    sumsDictionary[key] = sumsDictionary[key]! + [sumsDictionary[key]!.first! + coin * numberOfCoins]
-                }
+        sumsDictionary[coin] = [coin] + sumsDictionary.values.flatMap {
+            $0.map {
+                $0 + coin
             }
-            sumsDictionary[coin] = sumsDictionary[coin] == nil ? [coin] : sumsDictionary[coin]! + [sumsDictionary[coin]!.first! + coin]
-//            print(sumsDictionary)
-            numberOfCoins += 1
         }
     }
-    print(sumsDictionary.values)
+    print(sumsDictionary.values.flatMap{ $0 })
     return 1
 }
 
-print(possibleSums(coins: [10, 50, 100], quantity: [1, 2, 1]))
+print(possibleSums(coins: [10, 50, 100], quantity: [1, 1, 1]))
+
+//let arr = [[20, 30], [50], [100, 200]].flatMap {
+//    return $0
+//    }.map {
+//        $0 * 0
+//}
+//print(arr)
