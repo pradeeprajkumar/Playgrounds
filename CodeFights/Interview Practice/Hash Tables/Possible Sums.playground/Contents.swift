a65c1b42ff6777a92cbabd36c7826ae773c2f422
Pradeep Rajkumar <>
2018-07-12 15:36:20 +0530

    Implemented the possible sums by clearing few test cases.

--- CodeFights/Interview Practice/Hash Tables/Possible Sums.playground/Contents.swift
+++ CodeFights/Interview Practice/Hash Tables/Possible Sums.playground/Contents.swift
@@ -1,4 +1,3 @@
-
 func possibleSums(coins: [Int], quantity: [Int]) -> Int {
     var sumsDictionary = [Int:[Int]]()
     for (coin, coinQuantity) in zip(coins, quantity) {
@@ -7,16 +6,19 @@ func possibleSums(coins: [Int], quantity: [Int]) -> Int {
                 $0 + coin
             }
         }
+        if coinQuantity > 1 {
+            var times = coinQuantity - 1
+            let coinArray = sumsDictionary[coin]!
+            while times > 0 {
+                sumsDictionary[coin] = coinArray + sumsDictionary[coin]!.map {
+                    return $0 + coin
+                }
+                times -= 1
+            }
+        }
     }
-    print(sumsDictionary.values.flatMap{ $0 })
-    return 1
+    print(Set(sumsDictionary.values.flatMap{ $0 }).sorted())
+    return Set(sumsDictionary.values.flatMap{ $0 }).count
 }
 
-print(possibleSums(coins: [10, 50, 100], quantity: [1, 1, 1]))
-
-//let arr = [[20, 30], [50], [100, 200]].flatMap {
-//    return $0
-//    }.map {
-//        $0 * 0
-//}
-//print(arr)
+print(possibleSums(coins: [10, 50, 100], quantity: [1, 2, 1]))
