620bfb243b952fefd6fc627accf3d815ac762891
Pradeep Rajkumar <>
2018-07-17 00:42:38 +0530

    Fibonacci problem solved.

---
+++ CodeFights/Tournaments/Fibonacci Number.playground/Contents.swift
@@ -0,0 +1,17 @@
+func fibonacciNumber(n: Int) -> Int {
+    guard n > 1 else {
+        return n
+    }
+    var prev1 = 0, prev2 = 1
+    var index = 2
+    var result = 0
+    while index < n {
+        result = prev1 + prev2
+        prev1 = prev2
+        prev2 = result
+        index += 1
+    }
+    return result
+}
+
+print(fibonacciNumber(n: 2))
