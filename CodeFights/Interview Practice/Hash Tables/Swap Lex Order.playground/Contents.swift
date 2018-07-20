a8e0e65ee63e783bcbd1af1beef0800053239995
Pradeep Rajkumar <>
2018-07-20 11:27:48 +0530

    Swap lex order problem solved. Few test cases cleared.

--- CodeFights/Interview Practice/Hash Tables/Swap Lex Order.playground/Contents.swift
+++ CodeFights/Interview Practice/Hash Tables/Swap Lex Order.playground/Contents.swift
@@ -1,14 +1,47 @@
 func swapLexOrder(str: String, pairs: [[Int]]) -> String {
+    guard pairs.first?.count == 2 else {
+        return str
+    }
     var dictionary: [String:Bool] = [:]
-    var tempString = ""
+    var newString = str
+    var stringRepeated = false
+
     while true {
-        if dictionary[tempString] == true {
-            return dictionary.keys.sorted().last!
-        } else {
-            dictionary
+        for pair in pairs {
+            
+            var charArray = Array(newString.characters)
+            let tempChar = charArray[pair.first!-1]
+            charArray[pair.first! - 1] = charArray[pair.last! - 1]
+            charArray[pair.last! - 1] = tempChar
+            newString = String(charArray)
+            
+            if newString == str {
+                //print(pair)
+                stringRepeated = true
+            } else if dictionary[newString] != true {
+                print(newString)
+                dictionary[newString] = true
+            }
+        }
+        if stringRepeated {
+//            print(newString, dictionary[newString])
+            break
         }
     }
-    return "dcba"
+    print("\n")
+    return dictionary.keys.sorted().last!
 }
+print(swapLexOrder(str: "abdc", pairs: [[1, 4], [3, 4]]))
 
-//print(swapLexOrder(str: "abcd", pairs: [[1, 4], [3, 4]]))
+/*
+var pair = [2,6]
+
+var stringValue = "abcdef"
+//    stringValue.prefix(pair.first - 1) + stringValue.
+var charArray = Array(stringValue)
+let tempChar = charArray[pair.first!]
+charArray[pair.first! - 1] = charArray[pair.last! - 1]
+charArray[pair.last! - 1] = tempChar
+var newString = String(charArray)
+//print(newString)
+*/
