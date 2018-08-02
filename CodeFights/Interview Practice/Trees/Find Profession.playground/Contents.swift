af68116ea7d322a1fef1f0da0b9bfc222d2e7343
Pradeep Rajkumar <>
2018-08-03 00:07:07 +0530

    Find profession recursive tree problem solved.

--- CodeFights/Interview Practice/Trees/Find Profession.playground/Contents.swift
+++ CodeFights/Interview Practice/Trees/Find Profession.playground/Contents.swift
@@ -1,21 +1,15 @@
 func findProfession(level: Int, pos: Int) -> String {
-    var index = 0
-    var parentQueue: [Int] = [1]
-    repeat {
-        var childQueue: [Int] = []
-        for profession in parentQueue {
-            if profession == 1 {
-                childQueue.append(1)
-                childQueue.append(0)
-            } else {
-                childQueue.append(0)
-                childQueue.append(1)
-            }
-        }
-        parentQueue = childQueue
-        index += 1
-    } while index < level
-    return parentQueue[pos-1] == 1 ? "Engineer" : "Doctor"
+    if level == 1 {
+        return "Engineer"
+    }
+    if findProfession(level: level-1, pos: (pos+1)/2) == "Engineer" {
+        //Engineer: left = 1, right = 2
+        return pos % 2 == 1 ? "Engineer" : "Doctor"
+    } else {
+        //Doctor: left = 1, right = 2
+        return pos % 2 == 1 ? "Doctor" : "Engineer"
+    }
 }
 
 print(findProfession(level: 25, pos: 3))
+
