728e8a1cf88d6ca6097e66bd872ceae560ce9bbb
Pradeep Rajkumar <>
2018-08-08 23:47:46 +0530

    Is diagonal matrix problem solved.

---
+++ CodeFights/Tournaments/Is Diagonal Matrix.playground/Contents.swift
@@ -0,0 +1,18 @@
+func isDiagonalMatrix(matrix: [[Int]]) -> Bool {
+    var columnIndex = 0
+    for i in 0 ..< matrix.count {
+        for j in 0 ..< matrix[i].count {
+            if columnIndex != j && matrix[i][j] != 0 {
+                //It should be zero                
+                return false
+            }
+        }
+        columnIndex += 1
+    }
+    return true
+}
+
+debugPrint(isDiagonalMatrix(matrix: [[1,0,0,0],
+                                     [0,5,0,0],
+                                     [0,0,0,0],
+                                     [0,0,0,1000]]))
