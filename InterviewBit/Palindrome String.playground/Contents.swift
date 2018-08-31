d5c68b5cf489eee1e9b2d7cea6dcc520e7480978
Pradeep Rajkumar <>
2018-08-31 19:25:36 +0530

    Palindrome reverse method.

--- InterviewBit/Palindrome String.playground/Contents.swift
+++ InterviewBit/Palindrome String.playground/Contents.swift
@@ -1,9 +1,9 @@
-import Foundation
-var str = "ABCBA"
-var str1 = "Hello, playground"
 func isPalindrome(_ A: String) -> Bool {
     return A == String(A.reversed())
 }
+
+var str = "ABCBA"
+var str1 = "Hello, playground"
 
 print(isPalindrome(str))
 print(isPalindrome(str1))
